046677a20096e340f383a7985a25d48ab13de052
Andriikryl <andreyk557@gmail.com>
2024-09-19 23:38:51 +0300

    Add left block

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,60 @@
 </script>
 
 <template>
-    <div></div>
+    <div class="w-full h-[100dvh] flex">
+      <div class="bg-[#4242b1] w-full h-full">
+        <div class="flex flex-col items-center mt-12">
+          <div class="h-[170px]"></div>
+          <div class="min-w-[300px]">
+            <div>
+              <button class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)] flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m6.8 13l2.9 2.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.213-.325T3.426 12t.063-.375t.212-.325l4.6-4.6q.275-.275.7-.275t.7.275t.275.7t-.275.7L6.8 11H20q.425 0 .713.288T21 12t-.288.713T20 13z"/></svg>
+                <span>
+                  Beck to home
+                </span>
+              </button>
+            </div>
+            <div class="h-[10px]"></div>
+            <h1 class="text-[16px] leading-[20px] text-[rgba(255,_255,_255,_0.6)]">Subscribe to Arcanum Basic</h1>
+            <div class="flex items-center gap-3">
+              <p class="text-[36px] leading-[46px] tracking-[-0.48px] text-[rgb(255,_255,_255)]">$296.00</p>
+              <span class="text-[16px] leading-[20px] text-[rgba(255,_255,_255,_0.6)]">per <br>
+                year</span>
+            </div>
+            <div class="h-[20px]"></div>
+            <div class="flex justify-between items-center">
+              <p class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">Arcanum Basic</p>
+              <p class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">$296.00</p>
+            </div>
+            <div class="h-[10px]"></div>
+            <div class="w-full h-[2px] bg-[rgba(255,_255,_255,_0.1)]"></div>
+            <div class="h-[50px]"></div>
+            <div class="flex justify-between items-center">
+              <p class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">Subtotal</p>
+              <p class="text-[17px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">$296.00</p>
+            </div>
+            <div class="h-[10px]"></div>
+            <div class="w-full h-[2px] bg-[rgba(255,_255,_255,_0.1)]"></div>
+            <div class="h-[30px]"></div>
+            <div class="flex justify-between items-center">
+              <p class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">Tax</p>
+              <p class="text-[17px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">Enter address to calculate
+              </p>
+            </div>
+            <div class="h-[10px]"></div>
+            <div class="w-full h-[2px] bg-[rgba(255,_255,_255,_0.1)]"></div>
+            <div class="h-[40px]"></div>
+            <div class="flex justify-between items-center">
+              <p class="text-[14px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">Total due today</p>
+              <p class="text-[21px] leading-[18px] overflow-ellipsis break-words text-[rgb(255,_255,_255)]">$296.00</p>
+            </div>
+            <div class="h-[10px]"></div>
+            <div class="w-full h-[2px] bg-[rgba(255,_255,_255,_0.1)]"></div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#FFF] w-full h-full"></div>
+    </div>
 </template>
 
 <style scoped>
